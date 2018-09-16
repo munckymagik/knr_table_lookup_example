e6aa5599b5b9d91d172a743b24ea4cc1f7504840
Dan Munckton <dan@cultivatehq.com>
2018-09-16 17:29:05 +0100

    Make it easy to disable a test

--- test.h
+++ test.h
@@ -3,9 +3,13 @@
 
 #include <assert.h>
 
+// A convenience wrapper to define a test block
 #define TEST(description, body)    \
     printf("%s: ", (description)); \
     { body }                       \
     puts("PASS");                  \
+
+// A convenience macro to disable a test block
+#define XTEST(_description, _body)
 
 #endif /* TEST_H */

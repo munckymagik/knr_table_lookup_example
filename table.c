789f8a75770e7dbb00ad26e2bc48d283083db2a0
Dan Munckton <dan@cultivatehq.com>
2018-09-16 17:29:33 +0100

    Implement hash storage

--- table.c
+++ table.c
@@ -5,17 +5,30 @@
 
 #include "test.h"
 
-#define HASH_SIZE 1
+#define HASH_SIZE 101
 
 struct nlist {
     char *name;
     char *defn;
 };
 
-static struct nlist *hash_table[HASH_SIZE];
+static struct nlist *hash_table[HASH_SIZE] = { 0 };
+
+typedef uint32_t hash_value_t;
+
+hash_value_t hash_function(const char *s) {
+    hash_value_t hash_value = 0;
+
+    while (*s != '\0') {
+        hash_value = *s + 31 * hash_value;
+        s++;
+    }
+
+    return hash_value % HASH_SIZE;
+}
 
 struct nlist *hash_lookup(char *name) {
-    struct nlist *np = hash_table[0];
+    struct nlist *np = hash_table[hash_function(name)];
 
     if (np == NULL) {
         return NULL;
@@ -42,8 +55,8 @@ struct nlist *hash_insert(char *name, char *defn) {
             goto error;
         }
 
-        assert(hash_table[0] == NULL && "Table already had an entry");
-        hash_table[0] = np;
+        hash_value_t hash_value = hash_function(name);
+        hash_table[hash_value] = np;
     } else {
         // Found. Free the value because we are going to overwrite it.
         free(np->defn);
@@ -90,6 +103,31 @@ int main() {
 
         assert(found != NULL && "found was NULL");
         assert(strcmp(found->name, "X") == 0 && "found->name was not X");
+        assert(strcmp(found->defn, "Z") == 0 && "found->defn was not Z");
+    });
+
+    TEST("The hash function: ", {
+        assert(hash_function("") == 0 && "Hash isn't 0 for the empty string");
+        assert(hash_function("\1") == 1 && "Hash isn't 1 for '\1'");
+        assert(hash_function("\1\1") == 32 && "Hash isn't 32 for '\1\1'");
+        assert(hash_function("\1\1\1") == 84 && "Hash isn't 84 for '\1\1\1'");
+        assert(hash_function("\1\1\1") == 84 && "Hash isn't deterministic for '\1\1\1'");
+    });
+
+    TEST("Add more than one entry: ", {
+        struct nlist *found = NULL;
+
+        hash_insert("X", "Y");
+        hash_insert("Y", "Z");
+
+        found = hash_lookup("X");
+        assert(found != NULL && "found for X was NULL");
+        assert(strcmp(found->name, "X") == 0 && "found->name was not X");
+        assert(strcmp(found->defn, "Y") == 0 && "found->defn was not Y");
+
+        found = hash_lookup("Y");
+        assert(found != NULL && "found for Y was NULL");
+        assert(strcmp(found->name, "Y") == 0 && "found->name was not Y");
         assert(strcmp(found->defn, "Z") == 0 && "found->defn was not Z");
     });
 

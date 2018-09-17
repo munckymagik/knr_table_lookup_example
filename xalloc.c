d80258f619ea83bd8d821ec007feb9b88e917683
Dan Munckton <dan@cultivatehq.com>
2018-09-17 10:17:19 +0100

    Technically strlen + 1 bytes are allocated for strings

--- xalloc.c
+++ xalloc.c
@@ -17,9 +17,9 @@ void * xmalloc(size_t size) {
 }
 
 char * xstrdup(const char *s) {
-    size_t size = strlen(s);
-    increment_stats(size);
-    return strdup(s);
+    size_t str_len = strlen(s);
+    increment_stats(str_len + 1);
+    return strndup(s, str_len);
 }
 
 void xfree(void *p, size_t size) {
@@ -28,7 +28,7 @@ void xfree(void *p, size_t size) {
 }
 
 void xfree_str(char *s) {
-    xfree(s, strlen(s));
+    xfree(s, strlen(s) + 1);
 }
 
 struct xalloc_stats xalloc_get_stats() {
